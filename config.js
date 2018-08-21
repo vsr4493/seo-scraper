d30e619907018f9ce2e020946077eb2afcb9fdf6
Vorcan <vsr4493@gmail.com>
2018-08-22 01:44:55 +0530

    chore: add comments to config for clarity

--- config.js
+++ config.js
@@ -1,14 +1,17 @@
 module.exports = {
+	// Specify pages to be targeted
 	pages: [
 		{
 			url: 'https://www.google.com',
 		},
 	],
 	options: {
+		// Filter out unnecessary link tags based on the 'rel' attribute
 		linkTags: [
 			'canonical', 'amphtml'
 		],
-		metaTags: []
+		// Filter out meta tags by attribute name | property
+		metaTags: ['viewport', 'keywords']
 	},
 	outputPath: 'output.json'
 }
